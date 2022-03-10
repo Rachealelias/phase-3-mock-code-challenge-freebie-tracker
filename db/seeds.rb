e05e4f7024359bdd0eb971ae2436f0cbf0d8fbf5
Racheal Amuneke <rachealsamuel708@gmail.com>
2022-03-10 15:52:33 -0500

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 puts "Creating companies..."
+
 Company.create(name: "Google", founding_year: 1998)
 Company.create(name: "Facebook", founding_year: 2004)
 Company.create(name: "Dunder Mifflin", founding_year: 2002)
@@ -17,5 +18,13 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+puts "Creating freebies..."
+Freebie.create(item_name: "Keychain", value: 2, company_id: 1, dev_id: 2)
+Freebie.create(item_name: "Keychain", value: 2, company_id: 2, dev_id: 2)
+Freebie.create(item_name: "Phone Charger", value: 10, company_id: 4, dev_id: 2)
+Freebie.create(item_name: "Pencil", value: 1, company_id: 2, dev_id: 1)
+Freebie.create(item_name: "Knife", value: 4, company_id: 2, dev_id: 3)
+Freebie.create(item_name: "Job Offer", value: 60_000, company_id: 3, dev_id: 4)
+Freebie.create(item_name: "Job Offer", value: 200_000, company_id: 2, dev_id: 2)
 
 puts "Seeding done!"
